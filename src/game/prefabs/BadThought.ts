b7dfcee4780d0255278f3da7c0b770fd1f4248fd
Mosheg <Mosheg@papaya.com>
2025-01-24 19:12:06 +0200

    BadThoughts

---
+++ src/game/prefabs/BadThought.ts
@@ -0,0 +1,34 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+interface BadThought {
+
+	 body: Phaser.Physics.Arcade.Body;
+}
+
+class BadThought extends Phaser.Physics.Arcade.Sprite {
+
+	constructor(scene: Phaser.Scene, x?: number, y?: number, texture?: string, frame?: number | string) {
+		super(scene, x ?? 343, y ?? 322, texture || "Obstacle_01", frame);
+
+		scene.physics.add.existing(this, false);
+		this.body.setSize(421, 542, false);
+		// awake handler
+		this.scene.events.once("scene-awake", () => this.awake());
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+	awake() {
+		this.play("Obstacle_02");
+	}	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
