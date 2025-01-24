361b20e23644ae55e87ef1929f4fdf6b01653906
david rubin <davidrubin@davids-MacBook-Air.local>
2025-01-24 13:14:04 +0200

    ratio

--- src/PhaserGame.ts
+++ src/PhaserGame.ts
@@ -4,6 +4,8 @@ import { LobbyScene } from "./lobby/LobbyScene.js";
 import { MobileScene } from "./mobile/MobileScene.js";
 const width = window.innerWidth;
 const height = window.innerHeight;
+const ratio = height / 2436;
+
 import { Scene } from "./game/Scene.js";
 
 const scenes = new URLSearchParams(window.location.search).has('edit-level')
@@ -13,8 +15,8 @@ const scenes = new URLSearchParams(window.location.search).has('edit-level')
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width,
-  height,
+  width: width / ratio,
+  height: 2436,
   scene: scenes,
   physics: {
     default: "arcade",
