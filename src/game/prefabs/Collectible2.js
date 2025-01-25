bc5073832c2f321e67a940662455d82344397171
david rubin <davidrubin@davids-MacBook-Air.local>
2025-01-25 05:29:50 +0200

    fixed collectibles

--- src/game/prefabs/Collectible2.js
+++ src/game/prefabs/Collectible2.js
@@ -7,18 +7,29 @@ import { CollectibleBase } from "../CollectibleBase";
 class Collectible2 extends CollectibleBase {
 
 	constructor(scene, x, y, texture, frame) {
-		super(scene, x ?? 0, y ?? 0, texture || "collectible2", frame);
+		super(scene, x ?? 0, y ?? 0, texture || "PickUp_02", frame);
 
 		this.scaleX = 0.2;
 		this.scaleY = 0.2;
 
 		/* START-USER-CTR-CODE */
 		// Write your code here.
+		if (this.variant === "random") {
+			Collectible2.count = (Collectible2.count + 1) % 3;
+			const index = Collectible2.count + 1;
+			this.setTexture(`PickUp_0${index}`);
+		}
+		else {
+			this.setTexture(`PickUp_0${this.variant}`);
+		}
 		/* END-USER-CTR-CODE */
 	}
 
-	/* START-USER-CODE */
+	/** @type {"random"|"1"|"2"|"3"} */
+	variant = "random";
 
+	/* START-USER-CODE */
+	static count = 0;
 	// Write your code here.
 
 	/* END-USER-CODE */
