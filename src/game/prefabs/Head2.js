25b898f10f9d98b4815c1b23e0cc852ff5996503
eli.broide <eli.broide@tipalti.com>
2025-01-28 03:15:21 +0200

    version

--- src/game/prefabs/Head2.js
+++ src/game/prefabs/Head2.js
@@ -94,7 +94,7 @@ class Head2 extends Phaser.GameObjects.Container {
 		head.setScale(0.2);
 		head.y += 150;
 
-		this.body.setDragX(100);
+		this.body.setDragX(70);
 
 		// Create a looping rotation tween
 		this.scene.tweens.add({
